56e4032969bdb1fe6229056cf9d49f84d315b477
Victor Hugo <110066926+VictorHSLito@users.noreply.github.com>
2025-04-02 00:05:25 -0300

    Time: 4 ms (94.55%), Space: 17.8 MB (86.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) == sorted(t):
-            return True
-        else:
+        if len(s) != len(t):
             return False
-                
-        
+        s_string = set(s)
 
-        
+        for char in s_string:
+            if t.count(char) != s.count(char):
+                return False
+        return True
+
