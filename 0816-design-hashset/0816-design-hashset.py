11aa6a5f1244fea4fd5eb93b73d5da1d69f58f58
Victor Hugo <110066926+VictorHSLito@users.noreply.github.com>
2025-04-23 20:16:20 -0300

    Time: 42 ms (63.73%), Space: 22.9 MB (61.17%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.py
+++ 0816-design-hashset/0816-design-hashset.py
@@ -1,31 +1,45 @@
 class MyHashSet:
 
+    class _Node:
+        def __init__(self, key, next=None):
+            self.key = key
+            self.next = next
+
     def __init__(self):
-        self.size = 0
-        self.values = set()
+        self._slots = [MyHashSet._Node(0) for _ in range(1000)]
+        self.size = len(self._slots)
+
+
+    def _hash_function(self, key: int) -> int:
+        return key % self.size
 
     def add(self, key: int) -> None:
-        if key not in self.values:
-            self.values.add(key)
-            self.size += 1
-            return self.values
-        else:
-            return
+        index = self._hash_function(key)
+        head = self._slots[index]
+        while head.next:
+            if head.next.key == key:
+                return
+            head = head.next
+        head.next = MyHashSet._Node(key)
 
     def remove(self, key: int) -> None:
-        if key in self.values:
-            self.values.remove(key)
-            self.size -= 1
-            return self.values
-        else:
-            return
+        index = self._hash_function(key)
+        head = self._slots[index]
+        while head.next:
+            if head.next.key == key:
+                head.next = head.next.next
+                return
+            head = head.next
+            
 
     def contains(self, key: int) -> bool:
-        if key in self.values:
-            return True
-        else:
-            return False
-
+        index = self._hash_function(key)
+        head = self._slots[index]
+        while head.next:
+            if head.next.key == key:
+                return True
+            head = head.next
+        return False
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
