e14d2823ef56167e6c4e5995ae57bafa118c3065
Victor Hugo <110066926+VictorHSLito@users.noreply.github.com>
2025-07-23 19:56:30 -0300

    Time: 20 ms (10.81%), Space: 49.4 MB (86.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,9 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int length = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
         
-        Arrays.sort(nums);
-    
-        return nums[length/2];
+        int result = 0, maxCount = 0;
+
+        for(int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) > maxCount) {
+                result = num;
+                maxCount = map.get(num);
+            }
+        }
+
+        return result;
     }
 }
