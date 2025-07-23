f2d24dc59b9dd342189e1484707f2e5c32d90fb1
Victor Hugo <110066926+VictorHSLito@users.noreply.github.com>
2025-07-23 18:51:54 -0300

    Time: 3 ms (54.99%), Space: 53.4 MB (20.74%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int length = nums.length;
+        
+        Arrays.sort(nums);
+    
+        return nums[length/2];
+    }
+}
