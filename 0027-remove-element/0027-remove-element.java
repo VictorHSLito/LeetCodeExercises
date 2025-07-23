eaeb44031c991fcb317ec4b4743ec9b7dac5aea6
Victor Hugo <110066926+VictorHSLito@users.noreply.github.com>
2025-07-23 18:40:36 -0300

    Time: 0 ms (100%), Space: 41.7 MB (97.21%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,35 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int j = nums.length - 1;
-        int count = 0;
+        int k = 0;
 
-        for (int i = 0; i <= j; i++) {
-            if (nums[i] == val) {
-                while (j > i && nums[j] == val) {
-                    j--; 
-                }
-
-                if (i >= j) {
-                    break;
-                }
-
-                else {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    i--;
-                    j--;
-                }
-            }
-            else {
-                count++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
-        for(int num: nums) {
-            System.out.println(num);
-        }
-
-        return count;
+        return k;
+        
     }
 }
